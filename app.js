61b18fbfeca7b1dd2a0d2fb7a23ee7c554c15ac8
gmahur7 <gm7017308@gmail.com>
2024-06-07 09:02:49 +0530

    again and

--- app.js
+++ app.js
@@ -12,6 +12,10 @@ const port = process.env.PORT || 5000
 app.use(cors())
 app.use(express.json())
 
+app.get('/',(req,resp)=>{
+    resp.status(200).send("Hello Joy, Backend Is Working Properly")
+})
+
 app.use("/api/user", userRoutes)
 
 app.listen(port, () => {
